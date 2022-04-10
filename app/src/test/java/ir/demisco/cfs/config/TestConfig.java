05b56098fef861bbab18b5bbf5d11ebb9b14fece
m.kazemi <m.kazemi@istd.com>
2022-04-10 10:13:48 +0430

    feature-fin-1207

--- app/src/test/java/ir/demisco/cfs/config/TestConfig.java
+++ app/src/test/java/ir/demisco/cfs/config/TestConfig.java
@@ -1,21 +1,9 @@
 package ir.demisco.cfs.config;
 
-import ir.demisco.cloud.core.security.model.DmsUserDetails;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 
 @Configuration
 public class TestConfig {
-
-    @Bean
-    public UserDetailsService inMemoryDBUserDetailService() {
-        DmsUserDetails userDetails = new DmsUserDetails();
-        userDetails.setUsername("test");
-        userDetails.setUserId(1000L);
-        userDetails.setOrganizationId(1L);
-        return new InMemoryUserDetailsManager(userDetails);
-    }
 
 }
