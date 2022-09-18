8647db275ef9b44dc507d8148ed0120015ed9ef1
m.kazemi <m.kazemi@istd.com>
2022-09-18 15:17:26 +0430

    feature-fin-1444

--- service/src/main/java/ir/demisco/cfs/service/api/FinancialAlternativeUsersService.java
+++ service/src/main/java/ir/demisco/cfs/service/api/FinancialAlternativeUsersService.java
@@ -2,7 +2,6 @@ package ir.demisco.cfs.service.api;
 
 import ir.demisco.cfs.model.dto.request.FinancialAlternativeUsersInputRequest;
 import ir.demisco.cfs.model.dto.response.FinancialAlternativeUsersOutputResponse;
-
 import java.util.List;
 
 public interface FinancialAlternativeUsersService {
