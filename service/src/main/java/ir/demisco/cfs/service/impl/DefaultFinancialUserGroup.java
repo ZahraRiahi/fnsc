18cd8d94e9699eaf8311e693e801201d24d8ae7b
m.kazemi <m.kazemi@istd.com>
2022-12-31 11:11:48 +0330

    feature-fin-1791

--- service/src/main/java/ir/demisco/cfs/service/impl/DefaultFinancialUserGroup.java
+++ service/src/main/java/ir/demisco/cfs/service/impl/DefaultFinancialUserGroup.java
@@ -1,13 +1,18 @@
 package ir.demisco.cfs.service.impl;
 
+import ir.demisco.cfs.model.dto.request.FinancialUserGroupInputModelRequest;
 import ir.demisco.cfs.model.dto.request.FinancialUserGroupInputRequest;
+import ir.demisco.cfs.model.dto.response.FinancialUserGroupOutputModelResponse;
 import ir.demisco.cfs.service.api.FinancialUserGroupService;
 import ir.demisco.cfs.service.repository.FinancialUserGroupRepository;
 import ir.demisco.cloud.core.middle.exception.RuleException;
+import ir.demisco.cloud.core.security.util.SecurityHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DefaultFinancialUserGroup implements FinancialUserGroupService {
@@ -32,5 +37,17 @@ public class DefaultFinancialUserGroup implements FinancialUserGroupService {
                 .setParameter("disableDate", financialUserGroupInputRequest.getDisableDate())
                 .setParameter("financialUserGroupId", financialUserGroupInputRequest.getUserGroupId()).executeUpdate();
         return true;
+    }
+
+    @Override
+    @Transactional(rollbackFor = Throwable.class)
+    public List<FinancialUserGroupOutputModelResponse> getFinancialUserGroup(FinancialUserGroupInputModelRequest financialUserGroupInputModelRequest) {
+        List<Object[]> financialUserGroupList = financialUserGroupRepository.getFinancialUserGroupByOrganizationId(SecurityHelper.getCurrentUser().getOrganizationId()
+                , financialUserGroupInputModelRequest.getFinancialGroupId());
+        return financialUserGroupList.stream().map(objects -> FinancialUserGroupOutputModelResponse.builder().applicationUserId(objects[0] == null ? null : Long.parseLong(objects[0].toString()))
+                .financialUserId(objects[1] == null ? null : Long.parseLong(objects[1].toString()))
+                .userName(objects[2] == null ? null : (objects[2].toString()))
+                .nickName(objects[3] == null ? null : objects[3].toString())
+                .build()).collect(Collectors.toList());
     }
 }
