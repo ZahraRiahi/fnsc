2cd9f556454541e1a07136bbfb10f59abc52edea
m.kazemi <m.kazemi@istd.com>
2023-01-01 11:13:42 +0330

    feature-fin-1547

--- service/src/main/java/ir/demisco/cfs/service/api/FinancialAlternativeUsersService.java
+++ service/src/main/java/ir/demisco/cfs/service/api/FinancialAlternativeUsersService.java
@@ -2,6 +2,7 @@ package ir.demisco.cfs.service.api;
 
 import ir.demisco.cfs.model.dto.request.FinancialAlternativeUsersInputRequest;
 import ir.demisco.cfs.model.dto.request.FinancialAlternativeUsersListRequest;
+import ir.demisco.cfs.model.dto.request.FinancialUserAlternativeInputModelRequest;
 import ir.demisco.cfs.model.dto.response.FinancialAlternativeUsersOutputResponse;
 import java.util.List;
 
@@ -9,4 +10,7 @@ public interface FinancialAlternativeUsersService {
     List<FinancialAlternativeUsersOutputResponse> getFinancialAlternativeUsers(FinancialAlternativeUsersInputRequest financialAlternativeUsersInputRequest);
 
     Boolean setAlternativeUserEndDate(FinancialAlternativeUsersListRequest financialAlternativeUsersListRequest);
+
+    Boolean setAlternativeSetDisableDate(FinancialUserAlternativeInputModelRequest financialUserAlternativeInputModelRequest);
+
 }
