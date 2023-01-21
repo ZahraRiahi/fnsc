afdafba02901293d784615ea9c8aba3702e8c2d2
m.kazemi <m.kazemi@istd.com>
2023-01-21 11:03:44 +0330

    feature-fin-1870

--- service/src/main/java/ir/demisco/cfs/service/repository/FinancialGroupRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/FinancialGroupRepository.java
@@ -1,6 +1,5 @@
 package ir.demisco.cfs.service.repository;
 
-import ir.demisco.cfs.model.entity.FinancialDocumentType;
 import ir.demisco.cfs.model.entity.FinancialGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
