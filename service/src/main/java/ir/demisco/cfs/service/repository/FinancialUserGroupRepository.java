1a40adef5f1ad7ea218ad4c60061b67dbb974826
m.kazemi <m.kazemi@istd.com>
2023-01-21 10:00:18 +0330

    feature-fin-1870

--- service/src/main/java/ir/demisco/cfs/service/repository/FinancialUserGroupRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/FinancialUserGroupRepository.java
@@ -3,7 +3,6 @@ package ir.demisco.cfs.service.repository;
 import ir.demisco.cfs.model.entity.FinancialUserGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
