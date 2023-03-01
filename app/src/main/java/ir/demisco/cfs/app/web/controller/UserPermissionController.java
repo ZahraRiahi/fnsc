72f424328de14122d1e4ba8c3a8a4d87e1fc7ab8
z.tajmirriahi <z.tajmirriahi@demisco.com>
2023-03-01 09:39:51 +0330

    feature-fin-1933

--- app/src/main/java/ir/demisco/cfs/app/web/controller/UserPermissionController.java
+++ app/src/main/java/ir/demisco/cfs/app/web/controller/UserPermissionController.java
@@ -3,6 +3,10 @@ package ir.demisco.cfs.app.web.controller;
 import ir.demisco.cfs.model.dto.request.FinancialSecUserPermissionScopeInputModelRequest;
 import ir.demisco.cfs.model.dto.request.FinancialUserPermissionInputModelRequest;
 import ir.demisco.cfs.service.api.UserPermissionService;
+import ir.demisco.cfs.service.impl.UserPermissionGridProvider;
+import ir.demisco.cloud.core.middle.model.dto.DataSourceRequest;
+import ir.demisco.cloud.core.middle.model.dto.DataSourceResult;
+import ir.demisco.cloud.core.middle.service.business.api.core.GridFilterService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,9 +19,13 @@ import java.util.List;
 @RequestMapping("/api-userPermission")
 public class UserPermissionController {
     private final UserPermissionService userPermissionService;
+    private final GridFilterService gridFilterService;
+    private final UserPermissionGridProvider userPermissionGridProvider;
 
-    public UserPermissionController(UserPermissionService userPermissionService) {
+    public UserPermissionController(UserPermissionService userPermissionService, GridFilterService gridFilterService, UserPermissionGridProvider userPermissionGridProvider) {
         this.userPermissionService = userPermissionService;
+        this.gridFilterService = gridFilterService;
+        this.userPermissionGridProvider = userPermissionGridProvider;
     }
 
     @PostMapping("/save")
@@ -28,5 +36,10 @@ public class UserPermissionController {
     @PostMapping("/delete")
     public ResponseEntity<Boolean> deleteUserPermission(@RequestBody FinancialSecUserPermissionScopeInputModelRequest financialSecUserPermissionScopeInputModelRequest) {
         return ResponseEntity.ok(userPermissionService.deleteUserPermission(financialSecUserPermissionScopeInputModelRequest));
+    }
+
+    @PostMapping("/get")
+    public ResponseEntity<DataSourceResult> getUserPermission(@RequestBody DataSourceRequest dataSourceRequest) {
+        return ResponseEntity.ok(gridFilterService.filter(dataSourceRequest, userPermissionGridProvider));
     }
 }
